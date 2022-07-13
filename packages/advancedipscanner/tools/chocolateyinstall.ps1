3b000a6e617ef41f5a2c517de0c9ce53c95ecd45
BouBou <boubouqc@bell.net>
2022-07-12 21:37:18 -0400

    2.5.4594.1.2

--- packages/advancedipscanner/tools/chocolateyinstall.ps1
+++ packages/advancedipscanner/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
-  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  SilentArgs             = '/VERYSILENT /NORESTART'
   url                    = 'https://download.advanced-ip-scanner.com/download/files/Advanced_IP_Scanner_2.5.4594.1.exe'
   checksum               = '26D5748FFE6BD95E3FEE6CE184D388A1A681006DC23A0F08D53C083C593C193B'
   checksumType           = 'sha256'
