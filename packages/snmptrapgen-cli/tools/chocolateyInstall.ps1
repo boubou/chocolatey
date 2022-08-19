eb78fa1e2edff9abbe7e70497d5167707108f434
BouBou <boubouqc@bell.net>
2022-08-18 22:11:46 -0400

    SNMPTrapGen

--- packages/snmptrapgen-cli/tools/chocolateyInstall.ps1
+++ packages/snmptrapgen-cli/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url64          = 'https://github.com/enzo1982/freac/releases/download/v1.1.2/freac-1.1.2b-windows-x64.zip'
-    Checksum64     = '6B0363CE216DF714E243DD4780C96357C4D95A4CF79F167DF287DB041C393DB3'
+    Url64          = 'https://dl.ezfive.com/snmpsoft-tools/SnmpTrapGen.zip'
+    Checksum64     = 'D06FA7CD5C661182C302D6A7AAC982C43E755577CCD2C5FCE6BBF0065B827B27'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
