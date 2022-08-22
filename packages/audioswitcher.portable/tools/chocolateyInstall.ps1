23fc7ddb5b5ff670013fdcb5f98335e5e0bdff3b
BouBou <boubouqc@bell.net>
2022-08-21 22:01:19 -0400

    .

---
+++ packages/audioswitcher.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'Software Ideas Modeler.portable'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    PackageName    = $packageName
+    Url64          = 'https://github.com/xenolightning/AudioSwitcher_v1/releases/download/1.8.0.142/AudioSwitcher.zip'
+    Checksum64     = 'C991AAA5747BCC06F31FA8196F2F06B5BBB560ABA737E41098218D4CB5841A25'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $toolsDir
+}
+
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage @packageArgs
