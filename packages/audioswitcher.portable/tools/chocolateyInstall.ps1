f9540834cad288eab40324ddd06f011392b7188c
BouBou <boubouqc@bell.net>
2022-08-23 12:20:11 -0400

    1.8.0.172

--- packages/audioswitcher.portable/tools/chocolateyInstall.ps1
+++ packages/audioswitcher.portable/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     PackageName    = $packageName
     Url64          = 'https://github.com/xenolightning/AudioSwitcher_v1/releases/download/1.8.0.142/AudioSwitcher.zip'
-    Checksum64     = 'C991AAA5747BCC06F31FA8196F2F06B5BBB560ABA737E41098218D4CB5841A25'
+    Checksum64     = 'E675B85A7C8465E02B58148F8D98B4ADAB78C3A7423160CA78D1237D8469ACE4'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
