5713311b253cf7aebb688076fa723154326e30fb
Sebastien Boulianne <sebastien.boulianne@bell.net>
2023-05-04 22:12:08 -0400

    .

---
+++ packages/synology-assistant/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'Synology Assistant*'
+  fileType       = 'EXE'
+  url            = 'https://global.synologydownload.com/download/Utility/Assistant/7.0.4-50051/Windows/synology-assistant-7.0.4-50051.exe?model=DS3612xs&bays=12&dsm_version=6.2.4&build_number=25556'
+  checksum       = '0858F796EB7FCEBE6B7108B247B28A47519E9A103CA57A66C6AD45161FE00249'
+  checksumType   = 'sha256'
+  silentArgs     = "/S"
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ packages/synology-assistant/tools/chocolateyuninstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Synology Note Station Client'
+  fileType      = 'EXE'
+  file          = $env:UserProfile + "\AppData\Local\NoteStation\Uninstall.exe"
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+$uninstalled = $false
+
+Uninstall-ChocolateyPackage @packageArgs
