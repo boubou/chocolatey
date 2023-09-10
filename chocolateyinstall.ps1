6a23baf80eb781862d10636d4f275702df0d4aa2
Sébastien Boulianne <sebastien.boulianne@bell.net>
2023-09-09 23:08:26 -0400

    Add files via upload

---
+++ chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'qfinder*'
+  fileType       = 'EXE'
+  url            = 'https://download.qnap.com/Storage/Utility/QNAPQfinderProWindows-7.9.1.0817.exe'
+  checksum       = '539A013F5D6F4BF96419DD254D7CD6CFABF8FF6FF6A1D71C8E0C99CD96782A65'
+  checksumType   = 'sha256'
+  silentArgs     = "/S"
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
