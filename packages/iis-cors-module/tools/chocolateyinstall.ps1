7e4ac570fbb3d8940b45091fb45cbf9e8b383af5
Sebastien Boulianne <sebastien.boulianne@bell.net>
2023-09-30 20:18:07 -0400

    1.0.1987.1

--- packages/iis-cors-module/tools/chocolateyinstall.ps1
+++ packages/iis-cors-module/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $url        = 'https://download.microsoft.com/download/2/F/2/2F259559-FC43-4B2C-B53F-DED3E9950912/IISCORS_amd64.msi'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'MSI'
+  fileType      = 'EXE'
   url           = $url
   softwareName  = 'IIS CORS Module'
   checksum 		= '8A0B60AF8435A266A67DD1DF713D380B6267376A0608203D8468A5792351BB24'
