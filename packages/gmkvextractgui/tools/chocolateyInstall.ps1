3c8facedf25986bbc8a759933e877e4cb3df363b
Sebastien Boulianne <sebastien.boulianne@bell.net>
2023-11-02 18:37:02 -0400

    2.64

--- packages/gmkvextractgui/tools/chocolateyInstall.ps1
+++ packages/gmkvextractgui/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     PackageName    = $packageName
     Url32bit       = 'https://sourceforge.net/projects/gmkvextractgui/files/latest/download'
-    Checksum32     = '507246122C1256B3D3086DEC9AF8D059D77F56A35A409BC16CADD9DEF54BBD24'
+    Checksum32     = 'FCBB0FA65878B7079D41934A387A58BA72F1FF96B47F8522E0F895CFC18C41B6'
     ChecksumType32 = 'sha256'
     Url64bit       = 'https://sourceforge.net/projects/gmkvextractgui/files/latest/download'
-    Checksum64     = '507246122C1256B3D3086DEC9AF8D059D77F56A35A409BC16CADD9DEF54BBD24'
+    Checksum64     = 'FCBB0FA65878B7079D41934A387A58BA72F1FF96B47F8522E0F895CFC18C41B6'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
