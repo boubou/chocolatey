1feecc5efd388ed5911526a2da1bda8c6700c151
Sebastien Boulianne <boubouqc@bell.net>
2023-12-23 20:33:21 -0500

    first commit

---
+++ packages/cozy-drive/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url64       = 'https://nuts.cozycloud.cc/download/channel/stable/win'
+$checksum64  = 'F83E0D28BD0765645B1C6298AFC6B948E621B774516BE315ADF38EC68FBF4868'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url64Bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = '/install /quiet /norestart'
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
