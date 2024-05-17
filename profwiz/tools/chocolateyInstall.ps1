4bcead1c97ef4d9328365d33a514eaa3edb6a860
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2024-05-17 12:42:12 -0400

    Add files via upload

---
+++ profwiz/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.forensit.com/Downloads/Profwiz.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  softwareName  = 'Profwiz*'
+  checksum      = '3F49A09D6B1CBBA079D928E5BC7D2F716F57AC150540E0CF37E4B4517310B988'
+  checksumType  = 'sha256'
+
+}
+
+Install-ChocolateyZipPackage @packageArgs
