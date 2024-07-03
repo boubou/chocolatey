1f534e2e95b394d58ded92f82df2012ea48e82cf
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2024-07-03 09:10:06 -0400

    Add files via upload
    6.16.15901

--- packages/jabra-direct/tools/chocolateyinstall.ps1
+++ packages/jabra-direct/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $PackageParameters   = Get-PackageParameters
 $urlPackage = 'https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.exe'
-$checksumPackage = 'F3CC6A0955D4909F2A35DB262F2630835F9E17FECFDBA3538AFF6281E27D8B71'
+$checksumPackage = '587966AA7EA47E9E24A2EA814B63FBBF8215DB42C8126C158D4A20AC2B235331'
 $checksumTypePackage = 'sha256'
 
 Import-Module -Name "$($toolsDir)\helpers.ps1"
