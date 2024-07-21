5c9b48a69fab2f757374e847a956c8317adc5a33
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2024-07-20 21:12:26 -0400

    Add files via upload

--- packages/windows-repair-toolbox/tools/ChocolateyInstall.ps1
+++ packages/windows-repair-toolbox/tools/ChocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url64bit       = 'https://windows-repair-toolbox.com/download/click.php?id=Windows_Repair_Toolbox'
-    Checksum64     = 'A9BC0CA4240C89C1B195E1ED4714CD89E93110BDBDA6395B9AD5774E95D4CC32'
+    Url64bit       = 'https://windows-repair-toolbox.com/files/Windows_Repair_Toolbox.zip'
+    Checksum64     = '48BA96F84EE41FE6F9613EC88A54600034C360583D6A36C5AE49DE577185BEE2'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
