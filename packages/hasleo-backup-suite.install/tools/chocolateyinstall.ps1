a70835f18480eee28c3b1f804ced82d6a40ed0c7
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2024-08-26 15:52:37 -0400

    4.9

---
+++ packages/hasleo-backup-suite.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.easyuefi.com/backup-software/downloads/Hasleo_Backup_Suite_Free.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'Hasleo Backup Suite*'
+  checksum      = '75604AC418A96BBAF86EB6088BA4A2EE6F3AAA99B2FF6D23485F150EFAAAE536'
+  checksumType  = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
