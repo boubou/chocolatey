a2a8932424c094d5912239b31e01bcdd4bbd7ea8
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2024-12-04 15:07:53 -0500

    Add files via upload

---
+++ packages/windows-11-media-creation-tool/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$exeName = "MediaCreationTool.exe"
+
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  PackageName  = $env:chocolateyinstall
+  FileFullPath = "$toolsDir\$exeName"
+  Url          = 'https://software-static.download.prss.microsoft.com/dbazure/888969d5-f34g-4e03-ac9d-1f9786c66749/mediacreationtool.exe'
+  Checksum     = 'AB9967C2CD345FFDBF3C4283D95BB23C77A82A2782F381634F5DFA48F2B75071'
+  ChecksumType = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
+
+Register-Application "$toolsDir\$exeName"
