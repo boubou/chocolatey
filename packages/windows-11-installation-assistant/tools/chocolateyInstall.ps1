b42fac530ce015fc1d4b749ff1bb163b85689fcc
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2024-12-26 16:48:32 -0500

    Add files via upload

---
+++ packages/windows-11-installation-assistant/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿#$ErrorActionPreference = 'Stop'
+$exeName = "Windows11InstallationAssistant.exe"
+
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  PackageName  = $env:chocolateyinstall
+  FileFullPath = "$toolsDir\$exeName"
+  Url          = 'https://download.microsoft.com/download/6/8/3/683178b7-baac-4b0d-95be-065a945aadee/Windows11InstallationAssistant.exe'
+  Checksum     = '188698D10B1F7B9710061EC95E0AEC55A0CB2239E622FA4F7FDD5D360D00A007'
+  ChecksumType = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
+
+Register-Application "$toolsDir\$exeName"
