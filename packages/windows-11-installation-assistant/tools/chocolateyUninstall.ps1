b6b7cb23d80786f61c0fac559b6283e932a7a470
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2024-12-26 16:48:15 -0500

    Delete packages/windows-11-installation-assistant directory

--- packages/windows-11-installation-assistant/tools/chocolateyUninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$exeName = "Windows11InstallationAssistant.exe"
-$AppPathKey = "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\$exeName"
-
-If (Test-Path $AppPathKey) {Remove-Item "$AppPathKey" -Force -Recurse -EA SilentlyContinue | Out-Null}
