fb8c4cbe8f1f9532a098881ea85f29c49b7e5283
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2024-12-27 19:25:20 -0500

    Add files via upload

---
+++ packages/hpimageassistant/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$url        = 'https://hpia.hpcloud.hp.com/downloads/hpia/hp-hpia-5.3.1.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'HP Image Assistant'
+  checksum 		= 'F6EC10F8185F445877116CE3E6E0FE55F746F98670762EBCF53DB301250F7887'
+  checksumType  = 'sha256'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  
+}
+
+Install-ChocolateyPackage @packageArgs
