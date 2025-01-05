e54c7039ac6f456c3a2868f000fe1526b40a2070
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2025-01-04 20:42:21 -0500

    Add files via upload

--- packages/hpimageassistant/tools/chocolateyinstall.ps1
+++ packages/hpimageassistant/tools/chocolateyinstall.ps1
@@ -1,16 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url        = 'https://hpia.hpcloud.hp.com/downloads/hpia/hp-hpia-5.3.1.exe'
+$packageName= 'HP Image Assistant'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'EXE'
-  url           = $url
-  softwareName  = 'HP Image Assistant'
-  checksum 		= 'F6EC10F8185F445877116CE3E6E0FE55F746F98670762EBCF53DB301250F7887'
-  checksumType  = 'sha256'
-  silentArgs    = "/S"
-  validExitCodes= @(0)
-  
+    PackageName    = $packageName
+    Url64          = 'https://hpia.hpcloud.hp.com/downloads/hpia/hp-hpia-5.3.1.exe'
+    Checksum64     = 'F6EC10F8185F445877116CE3E6E0FE55F746F98670762EBCF53DB301250F7887'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $toolsDir
 }
 
-Install-ChocolateyPackage @packageArgs
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage @packageArgs
