29864d256919cfe789c47c59cbce2de94d89fc91
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2025-06-24 19:02:37 -0400

    Add files via upload
    2.10.0

--- packages/gmkvextractgui/tools/chocolateyInstall.ps1
+++ packages/gmkvextractgui/tools/chocolateyInstall.ps1
@@ -4,12 +4,9 @@ $toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url32bit       = 'https://sourceforge.net/projects/gmkvextractgui/files/latest/download'
-    Checksum32     = 'DC6638979B6F0E155974223E36ACA3E8D3E486EC2EB68DC4C6038564BB938FC2'
-    ChecksumType32 = 'sha256'
-    Url64bit       = 'https://sourceforge.net/projects/gmkvextractgui/files/latest/download'
-    Checksum64     = 'DC6638979B6F0E155974223E36ACA3E8D3E486EC2EB68DC4C6038564BB938FC2'
-    ChecksumType64 = 'sha256'
+    Url            = 'https://github.com/Gpower2/gMKVExtractGUI/releases/download/v2.10.0/gMKVExtractGUI.v2.10.0.7z'
+    Checksum       = 'F6006117884E1ACB97885A394458D70C143BAC816EBCB4FAD0A3DF7965A5AAAB'
+    ChecksumType   = 'sha256'
     UnzipLocation  = $toolsDir
 }
 
