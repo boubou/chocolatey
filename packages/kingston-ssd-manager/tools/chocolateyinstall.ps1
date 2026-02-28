6c68a71fa255d810dfba2d3e10de710da5fc6df9
Sébastien Boulianne <10491809+boubou@users.noreply.github.com>
2026-02-27 20:13:47 -0500

    Add files via upload

--- packages/kingston-ssd-manager/tools/chocolateyinstall.ps1
+++ packages/kingston-ssd-manager/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  url                    = 'https://media.kingston.com/support/downloads/KSM_setup_1.1.2.6.exe'
-  checksum               = '21110878E93FD7A15EFD3667103BDED7E67EB87AC83DBEB55464CCDC5C0442F1'
+  url                    = 'https://media.kingston.com/support/downloads/KSM_setup_1.5.6.1.exe'
+  checksum               = '1219AA31118E3F1309213BDE1585CC470DBBC13FF197CE3C37235102931840B4'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
